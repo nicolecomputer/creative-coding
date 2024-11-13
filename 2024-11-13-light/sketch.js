9502ffbda1a448fd858b54ef103ccb0301e05251
Nicole Watts <76132106+nicolecomputer@users.noreply.github.com>
2024-11-13 14:30:04 -0500

    back to sleep

---
+++ 2024-11-13-light/sketch.js
@@ -0,0 +1,93 @@
+let lights = [];
+
+function setup() {
+  createCanvas(400, 400);
+
+  for (let i = 0; i < 22; i++) {
+    lights.push({
+      position: createVector(random(0, 400), random(0, 220)),
+      offset: createVector(random(-0.6, -1.5), random(0, 0.8)),
+      life: random(0, 200),
+      size: randomGaussian(70, 14),
+      color: random([
+        "#FFF4D9",
+        "#FFF4D9",
+        "#FFF4D9",
+        "#FFF4D9",
+        "#FFF4D9",
+        "#FFF4D9",
+        "#FFF4D9",
+        "#FFF4D9",
+        "#FFF4D9",
+        "#FFF4D9",
+        "#FFF4D9",
+        "#FF0000",
+        "#FF0000",
+        "#FF0000",
+        "#7CFFAE",
+      ]),
+    });
+  }
+}
+
+function update() {
+  for (const light of lights) {
+    light.life -= randomGaussian(0.4, 0.4);
+    light.position = light.position.add(
+      createVector(randomGaussian(light.offset.x, 0.2), light.offset.y)
+    );
+
+    if (light.position.x < 0) {
+      light.position.x = 400;
+    }
+    if (light.position.y > height) {
+      light.position.y = random(0, 80);
+    }
+  }
+}
+
+function signature() {
+  const fillColor = color("#EEF2E4");
+  fillColor.setAlpha(80);
+  fill(fillColor);
+
+  const textColor = color("red");
+  textColor.setAlpha(80);
+  strokeWeight(2);
+  stroke(textColor);
+  text("NW", 370, 390);
+}
+
+function draw() {
+  update();
+
+  background("black");
+
+  for (const light of lights) {
+    const c = color(light.color);
+    c.setAlpha(map(cos(light.life / 10), 0, 1, 40, 220));
+
+    fill(c);
+    noStroke();
+    circle(light.position.x, light.position.y, light.size);
+  }
+
+  stroke("white");
+  strokeWeight(2);
+
+  fill("rgba(0,0,0,0.7)");
+  rect(20, 280, 360, 80);
+
+  fill("white");
+  noStroke();
+  textSize(14);
+
+  textStyle("bold");
+  text("oh my love,", 30, 310);
+
+  textStyle("normal");
+  text("we'll be there soon,", 60, 330);
+  text("go back to sleep,", 90, 350);
+
+  signature();
+}
