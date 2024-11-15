4b8dafa0e597e27a920054445ca68f7fc2ab9f5d
Nicole Watts <76132106+nicolecomputer@users.noreply.github.com>
2024-11-14 23:03:21 -0500

    more bouncing balls

---
+++ 2024-11-14-bouncing-ball-ii/sketch.js
@@ -0,0 +1,56 @@
+let balls = [];
+let canvas = {
+  width: 500,
+  height: 400,
+};
+
+function setup() {
+  createCanvas(canvas.width, canvas.height);
+
+  const colors = ["#06D6A0", "#1B9AAA", "#EF476F", "#FFC43D"];
+  for (let i = 0; i < 12; i++) {
+    let size = randomGaussian(60, 20);
+    balls.push({
+      position: createVector(
+        random(size, canvas.width - size),
+        random(size, canvas.height - size)
+      ),
+      speed: createVector(randomGaussian(2, 2), randomGaussian(2, 2)),
+      size: size,
+      color: colors[i % colors.length],
+    });
+  }
+}
+
+function update() {
+  for (const ball of balls) {
+    ball.position.add(ball.speed);
+
+    if (
+      ball.position.x + ball.size / 2 > canvas.width ||
+      ball.position.x - ball.size / 2 < 0
+    ) {
+      ball.speed.mult(createVector(-1, 1));
+    }
+    if (
+      ball.position.y + ball.size / 2 > canvas.height ||
+      ball.position.y - +ball.size / 2 < 0
+    ) {
+      ball.speed.mult(createVector(1, -1));
+    }
+  }
+}
+
+function draw() {
+  update();
+
+  background("#F8FFE5");
+
+  for (const ball of balls) {
+    noStroke();
+    const targetColor = color(ball.color);
+    targetColor.setAlpha(140);
+    fill(targetColor);
+    circle(ball.position.x, ball.position.y, ball.size);
+  }
+}
